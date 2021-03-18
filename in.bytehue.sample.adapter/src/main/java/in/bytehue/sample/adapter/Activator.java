f65dab4565e9a55f3b8bc3a8c8acf0da07efa8e1
Amit Kumar Mondal <admin@amitinside.com>
2021-03-18 10:44:23 +0100

    Initial Repo

---
+++ in.bytehue.sample.adapter/src/main/java/in/bytehue/sample/adapter/Activator.java
@@ -0,0 +1,20 @@
+package in.bytehue.sample.adapter;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+    @Override
+    public void start(final BundleContext context) throws Exception {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void stop(final BundleContext context) throws Exception {
+        // TODO Auto-generated method stub
+
+    }
+
+}
